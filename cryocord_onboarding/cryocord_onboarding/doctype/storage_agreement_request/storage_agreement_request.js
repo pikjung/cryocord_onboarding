bb2bd0380ae22674c19f4170d2ca9518634cd9e5
Fikri Darmawan <fikridarmawan@sokkakreatif.com>
2026-07-02 13:31:16 +0700

    feat: add storage agreement request with child table

---
+++ cryocord_onboarding/cryocord_onboarding/doctype/storage_agreement_request/storage_agreement_request.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2026, Fikri and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Storage Agreement Request", {
+// 	refresh(frm) {
+
+// 	},
+// });
